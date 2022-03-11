2a1358b135dee5c9c4193a29ee56eb8478a2d1e4
Ethan-Rei <ethanrei@my.yorku.ca>
2022-03-11 15:29:01 -0500

    test comment for commit

--- app/src/main/java/com/example/passwordprotecteddiaryapp/MainActivity.java
+++ app/src/main/java/com/example/passwordprotecteddiaryapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Test comment for commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
